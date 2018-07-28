c9d1c2912e7be43e7e2698c6f9d86f2a9786ee9a
arbertrary <unk>
2018-07-29 00:40:08 +0200

    Exercise 5

--- src/main/scala/chapter1/cat/Cat.scala
+++ src/main/scala/chapter1/cat/Cat.scala
@@ -19,14 +19,23 @@ object Cat {
     }
   }
 
+  // Exercise 4
   implicit val catShow: Show[Cat] = {
     cat =>
       val name = cat.name.show
       val age = cat.age.show
       val color = cat.color.show
       s"$name is a $age year-old $color chapter1.cat."
-
   }
+
+  //  Exercise 5
+  implicit val catEq: Eq[Cat] = Eq.instance({
+    (cat1, cat2) =>
+      cat1.name === cat2.name &&
+        cat1.age === cat2.age &&
+        cat1.color === cat2.color
+  })
+
 
   def main(args: Array[String]): Unit = {
     //    Without PrintableSyntax:
@@ -38,5 +47,18 @@ object Cat {
 
     // using Show[Cat] resp. catShow as defined above
     println(cat.show)
+
+
+    val cat1 = Cat("Garfield", 38, "orange and black")
+    val cat2 = Cat("Heathcliff", 33, "orange and black")
+    println(cat1 === cat2)
+    println(cat1 =!= cat2)
+
+
+    val optionCat1 = Option(cat1)
+    val optionCat2 = Option.empty[Cat]
+    println(optionCat1 === optionCat2)
+    println(optionCat1 =!= optionCat2)
+
   }
 }
